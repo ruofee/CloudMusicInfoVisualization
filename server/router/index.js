71475b2efc12e9d7154aef66c1e0e3acb89db978
陈若枫 <768047505@qq.com>
2019-04-08 01:18:59 +0800

    增加获取歌手详情接口

--- server/router/index.js
+++ server/router/index.js
@@ -5,6 +5,7 @@ const getUser = require('./getUser');
 const getUserInfo = require('./getUserInfo');
 const getPlayList = require('./getPlayList');
 const getLikeList = require('./getLikeList');
+const getArtist = require('./getArtist');
 const getSongDetail = require('./getSongDetail');
 const getCommentHot = require('./getCommentHot');
 
@@ -25,6 +26,10 @@ module.exports = app => {
   // 获取用户歌单列表
 
   getPlayList(router);
+
+  // 根据歌手id获取详情
+
+  getArtist(router);
 
   // 根据id获取用户喜欢的歌曲
 
