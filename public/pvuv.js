368f18639f7a789b6b633fd8f29667921e49448f
陈若枫 <768047505@qq.com>
2019-04-13 19:11:40 +0800

    引入console彩蛋文件以及百度统计代码

---
+++ public/pvuv.js
@@ -0,0 +1,7 @@
+let _hmt = _hmt || [];
+(function() {
+  let hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9345e3d4d503fc4069b222d9fa066474";
+  let s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
